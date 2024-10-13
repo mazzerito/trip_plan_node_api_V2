10bd560024688ffe3147716ef6d84cbb238bc09f
IMaxio <mazzersioter@gmail.com>
2024-10-14 00:58:39 +0700

    v5

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
 const { Sequelize } = require("sequelize");
 
 // For PostgreSQL or MySQL:
-const sequelize = new Sequelize("trip_plan_db", "postgres", "150202", {
-    host: "localhost",
+const sequelize = new Sequelize("trip_plan_db", "trip_plan", "qcFIa74S5p6DToEJSGgsmlUPYAfWsVyI", {
+    host: "dpg-cs60jm56l47c73f9a1ig-a",
     dialect: "postgres", 
 });
 
+// const sequelize = new Sequelize("trip_plan_db", "postgres", "150****", {
+//     host: "localhost",
+//     dialect: "postgres", 
+// });
 module.exports = sequelize;
